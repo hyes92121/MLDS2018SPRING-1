cdb4bc815ec5fd9009cab1272cfbfb422a53cdcf
hyes92121 <hyes92121@gmail.com>
2018-06-08 22:07:11 +0800

    fix sh bug

--- hw3/run_cgan.sh
+++ hw3/run_cgan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-wget -cO  https://www.dropbox.com/s/u7z71abtzfzexs0/model.tar?dl=0 > model.tar
+wget -cO  - https://www.dropbox.com/s/u7z71abtzfzexs0/model.tar?dl=0 > model.tar
 python3 infer.py $1
 
